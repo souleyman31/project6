4fe502e50d118c009e33b97a57abe90959a0fb20
Souleyman <soulmandra@gmail.com>
2021-08-30 22:21:57 +0200

    Connexion à MongoDB Atlas

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+require("dotenv/config");
+
 const app = express();
+
+//MONGOOSE CONNECT
+// mongoose.set("useNewUrlParser", true);
+// mongoose.set("useFindAndModify", false);
+// mongoose.set("useCreateIndex", true);
+
+mongoose
+  .connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
