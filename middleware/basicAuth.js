f22718c62c158c241fd2952e63cb95d24bb59a61
Ciara <soulmandra@gmail.com>
2021-09-08 15:42:49 +0200

    verification

--- middleware/basicAuth.js
+++ middleware/basicAuth.js
@@ -1,10 +1,11 @@
 const Sauce = require("../models/Sauce");
+
 module.exports = (req, res, next) => {
 	// console.log(req.userFromToken);
 	Sauce.findOne({ _id: req.params.id }).then(sauce => {
 		if (sauce.userId !== req.userFromToken) {
-			res.status(403).json({ msg: " Vous n'êtes pas le propriétaire" });
-			next();
+			return res.status(403).json({ msg: " Vous n'êtes pas le propriétaire" });
 		}
+		next();
 	});
 };
